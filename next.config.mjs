07117bd83450431ccccfa9b9ba99bfebf6420804
sina <kokibernard123@gmail.com>
2024-10-26 14:00:46 -0700

    english config 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/khatt',
-  i18n: {
-    locales: ['en', 'ar'], // Add your supported languages here
-    defaultLocale: 'en',
-  },
 };
 
 export default nextConfig;
