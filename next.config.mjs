74060a7f574ea880e6a69de6afecb8d6b4592e08
sina <kokibernard123@gmail.com>
2024-10-26 14:17:16 -0700

    english config 8

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/khatt',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/fa',
+        permanent: true
+      },
+    
+    ];
+  },
 };
 
 export default nextConfig;
