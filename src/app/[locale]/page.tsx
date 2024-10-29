ee588e5f49521a614152fa3a4aab8cbcae39500b
sina <kokibernard123@gmail.com>
2024-10-29 16:29:34 +0330

    work on performance 8

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -77,7 +77,7 @@ const Page = () => {
         <div className={" w-full !max-w-[1240px] mx-auto px-[24px]"}>
           <SimpleParallax scale={1.2}>
             <Image src={mainSectionBanner} alt="image"
-                   className={"mx-auto aspect-auto max-md:h-[500px] max-md:object-cover "}/>
+                   className={"mx-auto max-md:h-[500px] max-md:object-cover "}/>
           </SimpleParallax>
         </div>
 
