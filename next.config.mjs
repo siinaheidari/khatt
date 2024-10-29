cb042bcfe3b3fe5cbdf0cee9be90dff461e0c1aa
sina <kokibernard123@gmail.com>
2024-10-29 17:13:14 +0330

    work on performance 17

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,7 @@
 const nextConfig = {
   productionBrowserSourceMaps: false,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        // Apply the header to all routes
-        source: '/(.*)', // This regex matches all paths
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' 'inline-speculation-rules';",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
