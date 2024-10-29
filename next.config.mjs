d9eb480d6253fd815ea0dd6f73b58c31f314bb0b
sina <kokibernard123@gmail.com>
2024-10-29 16:40:44 +0330

    work on performance 11

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,6 @@
 const nextConfig = {
     productionBrowserSourceMaps: false,
     swcMinify: true,
-    
-    
-    source: '/(.*)',
-    headers: [
-      {
-        key: 'Content-Security-Policy',
-        value: 'default-src \'self\'; script-src \'self\' \'wasm-unsafe-eval\' \'unsafe-inline\' \'inline-speculation-rules\';',
-      },
-    ],
   
   };
 
